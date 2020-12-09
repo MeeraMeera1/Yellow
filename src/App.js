cea895be13e46113bfb1be1c814151d48dbc6932
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-09 18:39:29 -0500

    First commit from morning

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ColorSquare from "./components/ColorSquare/index";
+
+
+function App() {
+  return (
+    <div>
+    <ColorSquare color={"0,0,255"} />
+    <ColorSquare color={"255,0,0"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ColorSquare/index.js
@@ -0,0 +1,15 @@
+//react component is a function that outputs something visual
+
+const ColorSquare = () => {
+  return (
+    <div
+      style={{
+        width: "200px",
+        height: "200px",
+        background: "rgb(0,0,255)",
+      }}
+    ></div>
+  );
+};
+
+export default ColorSquare;
